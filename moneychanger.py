8153ae7b77fd93888bf4b95c32c7d377e3731c55
jonfernandes <jonfernandes@gmail.com>
2024-12-23 22:22:02 +0000

    langsmith support

--- moneychanger.py
+++ moneychanger.py
@@ -2,12 +2,11 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 import openai
-
 import requests
 import json
 
 from openai import OpenAI
-
+from langsmith import traceable
 
 
 # Load environment variables from .env file
@@ -17,6 +16,10 @@ load_dotenv()
 EXCHANGE_API = "https://api.exchangerate-api.com/v4/latest/USD"
 EXCHANGERATE_API_KEY = os.getenv("EXCHANGE_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "streamlit-moneychanger"
+MODEL_PROVIDER = "openai"
+MODEL_NAME = "gpt-4o"
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 # Streamlit app title
@@ -53,17 +56,20 @@ tools = [
     }
 ]
 
-
+@traceable
 def get_exchange_rate(base: str, target: str, amount: str) -> dict:
     """Return conversion result from exchangerate-api.com"""
     url = f"https://v6.exchangerate-api.com/v6/{EXCHANGERATE_API_KEY}/pair/{base}/{target}/{amount}"
     exchange_response = json.loads(requests.get(url).text)
     return f'{base} {amount} is {target} {exchange_response["conversion_result"]:.2f}'
 
+@traceable(
+    metadata={"model_name": MODEL_NAME, "model_provider": MODEL_PROVIDER}
+)
 def call_llm(text, tools=None):
     try:
         completion = client.chat.completions.create(
-        model="gpt-4o",
+        model=MODEL_NAME,
         messages=[{"role": "user", "content": text}],
         tools=tools,
         )
@@ -72,20 +78,25 @@ def call_llm(text, tools=None):
     else:
         return completion
 
+@traceable
+def run_moneychanger(user_input=None, tools=None):
+    completion = call_llm(user_input, tools)
+    print(f"--> {completion.choices[0].finish_reason}")
+    if completion.choices[0].finish_reason == "tool_calls":
+        response_arguments = json.loads(completion.choices[0].message.tool_calls[0].function.arguments)
+        base = response_arguments['base']
+        target = response_arguments['target']
+        amount = response_arguments['amount']
+        st.write(get_exchange_rate(base, target, amount))
+    elif completion.choices[0].finish_reason == "stop":
+        st.write(f"(Function calling not used): {completion.choices[0].message.content}")
+    else:
+        st.write("NotImplemented")
+
+
 # Button to submit the input
 if st.button("Submit"):
     if user_input:
-        completion = call_llm(user_input, tools)
-        print(completion)
-        if completion['choices'][0]['message']['finish_reason'] == "tool_calls":
-            response_arguments = json.loads(completion.choices[0].message.tool_calls[0].function.arguments)
-            base = response_arguments['base']
-            target = response_arguments['target']
-            amount = response_arguments['amount']
-            st.write(get_exchange_rate(base, target, amount))
-        elif completion['choices'][0]['message']['finish_reason'] == "stop":
-            st.write(f"(Function calling not used): {completion}")
-        else:
-            st.write("NotImplemented")
+        run_moneychanger(user_input, tools)
     else:
         st.write("Please enter a value.")
